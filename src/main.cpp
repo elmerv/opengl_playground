fb8334393c7f166a40bd92dd8e10fd005639bb84
Elmer Vasquez <elfvasqu@ucsc.edu>
2023-01-04 02:40:46 -0800

    fixed the error with glClear not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define GLFW_INCLUDE_NONE
+#define GL_SILENCE_DEPRECATION // Important to define before OpenGL headers.
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -10,9 +10,9 @@ void error_callback(int error, const char* description)
 }
 
 int main(){
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     if (!glfwInit()){
         // Initialization failed
@@ -26,8 +26,21 @@ int main(){
     }   
     glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window)){
-    // Keep running
+        // Keep running
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+
+        glViewport(0, 0, width, height);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
     glfwTerminate();
